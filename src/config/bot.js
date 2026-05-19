c56fe4314c6fa9fc51c15fdfffb31702a59c72e4
luluffyy777 <lululh01@gmail.com>
2026-05-19 10:56:16 +0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Im Pamine, the owner of this server!",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
